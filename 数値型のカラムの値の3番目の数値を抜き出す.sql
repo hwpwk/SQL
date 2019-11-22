d0d59cfa454d4ec1376a384de24235d0623476c5
hwpwk <34832156+hwpwk@users.noreply.github.com>
2019-11-22 19:54:01 +0900

    substring(cast(HOME_PHONE_NO as nvarchar), 3, 1)

---
+++ 数値型のカラムの値の3番目の数値を抜き出す.sql
@@ -0,0 +1,7 @@
+﻿-- Select third digit of home phone for each customer with valid home phone number from Tbl_customer table:
+select 
+  HOME_PHONE_NO,
+  substring(cast(HOME_PHONE_NO as nvarchar), 3, 1) as third_digit_of_home_phone
+from
+  Tbl_customer
+;
