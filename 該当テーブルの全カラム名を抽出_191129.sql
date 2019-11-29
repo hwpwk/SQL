7ed372fe0830642772d7710873584d48ac2f78b9
hwpwk <34832156+hwpwk@users.noreply.github.com>
2019-11-29 17:06:03 +0900

    SELECT syscolumns.name FROM syscolumns INNER JOIN

---
+++ 該当テーブルの全カラム名を抽出_191129.sql
@@ -0,0 +1,12 @@
+﻿--  sysobjects.name にカラム名を抽出したい該当のテーブル名を指定する
+SELECT
+    syscolumns.name
+FROM
+    syscolumns
+INNER JOIN sysobjects ON
+    sysobjects.id = syscolumns.id
+WHERE
+    sysobjects.name = 'table_name'
+;
+
+-- 参考：https://lightgauge.net/database/sqlserver/1491/
