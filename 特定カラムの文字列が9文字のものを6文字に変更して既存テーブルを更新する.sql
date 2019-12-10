e3e6964f8f4975c49e4a67453c01f99b8664d91c
hwpwk <34832156+hwpwk@users.noreply.github.com>
2019-12-10 21:16:17 +0900

    SET [x] =SUBSTRING([x], 1, 6) WHERE len([x])=9

---
+++ 特定カラムの文字列が9文字のものを6文字に変更して既存テーブルを更新する.sql
@@ -0,0 +1,7 @@
+﻿-- 特定カラムの文字列が9文字のものを6文字に変更して既存テーブルを更新する
+
+UPDATE [dbo].[C_1704]
+SET [コード] =SUBSTRING([コード], 1, 6) 
+WHERE len([コード])=9
+
+/* 参考:https://stackoverflow.com/questions/18570965/update-substring-of-a-column */
