6129f6be98a5c79da4cdfeb013a1b1ba61e279e3
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-02-28 15:19:32 +0900

    WHERE [日]=(SELECT MIN([日]) FROM t) OR [日]

---
+++ 最古日付と最新日付のレコードのみ抽出する.sql
@@ -0,0 +1,12 @@
+﻿-- 最古日付と最新日付のレコードのみ抽出する
+-- 参考：https://teratail.com/questions/155602
+SELECT
+    [NO]
+    ,[日付]
+    ,[売上]
+FROM
+    tmp2
+WHERE
+  [日付] = (SELECT MIN([日付]) FROM tmp2)
+  OR
+  [日付] = (SELECT MAX([日付]) FROM tmp2)
