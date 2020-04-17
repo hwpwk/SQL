48a23f0f978499cb9ffa0510152b50b71b4e6440
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-04-17 15:53:02 +0900

    SELECT FORMAT(SUM([売上高]),'N')

---
+++ 数値を3桁区切りの文字列に変換する.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+    FORMAT(SUM([当月＿売上高]),'N') AS 当月売上高合計
+FROM
+    [Order_FINAL]
+;
+
+--→874,901,798,491.00というように3桁区切りの文字列に変換される
+-- 参考：https://ichiroku11.hatenablog.jp/entry/2014/01/21/234846 
