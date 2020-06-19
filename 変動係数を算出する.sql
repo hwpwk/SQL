da1af298e35c9c5883f7539a155bf487f56bdb1e
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-06-19 17:15:55 +0900

    COALESCE(STDEVP([日数]) / NULLIF(AVG([日数]), 0), 0)

---
+++ 変動係数を算出する.sql
@@ -0,0 +1,14 @@
+﻿SELECT
+    [会社コード]
+    ,AVG([仕入債務]) AS [仕入債務_平均]
+    ,AVG([仕入債務回転日数]) AS [仕入債務回転日数_平均]
+    ,COALESCE(NULLIF(STDEVP([仕入債務]), 0), 0) AS [仕入債務_母標準偏差]
+    ,COALESCE(NULLIF(STDEVP([仕入債務回転日数]), 0), 0) AS [仕入債務回転日数_母標準偏差]
+    ,COALESCE(STDEVP([仕入債務]) / NULLIF(AVG([仕入債務]), 0) , 0) AS [仕入債務_変動係数]
+    ,STDEVP([仕入債務]) / NULLIF(AVG([仕入債務]), 0) AS [仕入債務_変動係数_NULL]
+    ,COALESCE(STDEVP([仕入債務回転日数]) / NULLIF(AVG([仕入債務回転日数]), 0), 0)AS [仕入債務回転日数_変動係数]
+    ,STDEVP([仕入債務回転日数]) / NULLIF(AVG([仕入債務回転日数]), 0) AS [仕入債務回転日数_変動係数_NULL]
+FROM
+    A
+GROUP BY
+    [会社コード]
