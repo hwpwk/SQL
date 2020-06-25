52c4fb8bda0042aa1fbd0de6424069c07f929a23
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-06-25 12:32:11 +0900

    YEAR(DATEADD(MONTH, -(@closing_month%12) ,[決算日]) )

---
+++ SQLServerで会計年度を算出する.sql
@@ -0,0 +1,13 @@
+﻿-- 参考：https://qiita.com/ShibuyaKosuke/items/f83f43898920daf01e7b
+
+DECLARE @closing_month INTEGER
+SET @closing_month=3 -- 決算月
+
+SELECT 
+    [決算日]
+    ,YEAR([決算日]) AS [決算年]
+    -- 決算月に合わせた会計年度を抽出
+    ,YEAR(DATEADD(MONTH, -(@closing_month%12) ,[決算日]) ) AS [会計年度]
+FROM
+    [Demo_FS].[ana].[連結財務諸表_PIVOT]
+;
