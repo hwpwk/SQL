f8f25eeee50bddcc3c5301e0e710ae651838ba6d
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-07-13 12:02:11 +0900

    SUM(CASE WHEN [日]<’2020-03-31’ THEN 1 ELSE 0 END)

---
+++ CASE WHEN式を使用して指定した日以前の件数と以後の件数を算出.sql
@@ -0,0 +1,6 @@
+﻿SELECT 
+    COUNT(*) AS [全数]	
+   ,SUM(CASE WHEN [決算日]<=’2020-03-31’ THEN 1 ELSE 0 END) AS [FY20Q4までの件数]
+   ,SUM(CASE WHEN [決算日]>’2020-03-31’ THEN 1 ELSE 0 END) AS [FY21Q1の件数]
+FROM
+    [Table]
