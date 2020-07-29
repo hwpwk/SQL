6291fc73dd93d64f248a06ee9876cc8082c3702c
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-07-30 08:02:42 +0900

    CASE WHEN [名] LIKE '% %'THEN REPLACE([名],' ', '' )

---
+++ CASE WHEN式を使用し文字列の全角スペースと半角スペースを削除.sql
@@ -0,0 +1,10 @@
+﻿SELECT 
+      [コード]
+      ,CASE
+          WHEN [名] LIKE '% %' THEN REPLACE([名],' ', '' )
+          WHEN [名] LIKE '%　%' THEN REPLACE([名],'　', '' )
+          ELSE [名]
+      END AS [名]
+FROM
+      tbl
+;
