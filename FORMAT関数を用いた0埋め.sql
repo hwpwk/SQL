5868b7805c74cb18f71990ee0e2d40cab6649b97
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-11-05 09:04:56 +0900

    LEN([コ])<2 THEN FORMAT(CAST([コ] AS INT),'00')

---
+++ FORMAT関数を用いた0埋め.sql
@@ -0,0 +1,10 @@
+﻿-- 6桁未満のコードについては「0」で埋める
+SELECT
+      [決算日]
+      -- FROMAT関数は文字列型のカラムを扱うことができないのでINTに変換している
+      ,CASE
+         WHEN LEN([コード])<6 THEN FORMAT(CAST([コード] AS INT), '000000')
+         ELSE [コード]
+      END AS [コード]
+FROM
+      [dbo].[tbl]
