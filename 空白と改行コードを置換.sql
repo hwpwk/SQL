f0b19406df1f9508df5ef424111def65d6db5bf8
hwpwk <34832156+hwpwk@users.noreply.github.com>
2020-11-24 20:12:03 +0900

    REPLACE([勘定科目コード], CHAR(10), '')

---
+++ 空白と改行コードを置換.sql
@@ -0,0 +1,8 @@
+﻿SELECT
+      REPLACE([勘定科目コード], ' ','')  -- 空白の削除
+      ,REPLACE([勘定科目コード], CHAR(10), '') -- 改行の削除(いろいろパターンがあり、CHAR(10)だけではないことに注意)
+FROM
+      tbl
+-- 参考
+-- https://sql55.com/query/remove-carriage-return.php
+-- https://sql-oracle.com/sqlserver/?p=1004
