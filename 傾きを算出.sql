101d16a21229d6278b06b39f9c07fddb885cc709
hwpwk <34832156+hwpwk@users.noreply.github.com>
2021-05-19 19:19:55 +0900

    AVG([DIV_x]*[DIV_y]) /NULLIF([VARP_x], 0)

---
+++ 傾きを算出.sql
@@ -0,0 +1,7 @@
+﻿-- 傾き=共分散÷分散
+-- 共分散 = (Xの偏差×Yの偏差)の平均(=(Xの偏差×Yの偏差)の合計÷個数)
+SELECT
+    AVG([DIV_x]*[DIV_y]) OVER(PARTITION BY [c1],[c2],[c3]) / NULLIF([VARP_x], 0) / NULLIF([AVG_y], 0) AS [Slope(adjusted)]
+    ,AVG([DIV_x]*[DIV_y]) OVER(PARTITION BY [c1],[c2],[c3]) / NULLIF([VARP_x], 0) AS [Slope]
+FROM
+    tmp
