903217dec6f18bd81b32935955b5aa5b94f0a736
hwpwk <34832156+hwpwk@users.noreply.github.com>
2021-05-26 18:51:41 +0900

    ROWSBETWEENUNBOUNDEDPRECEDINGANDUNBOUNDEDFOLLOWING

---
+++ 最古日付のデータと最新日付のデータを取得.sql
@@ -0,0 +1,10 @@
+﻿SELECT
+   -- 最古日付時点の金額を取得
+    FIRST_VALUE([SUM_Amount in LC]) 
+　　　OVER(PARTITION BY [col1], [col2] ORDER BY [Date]) AS [MinDayAmount]
+　-- 最新年月の末日時点の金額を取得(年度毎)
+　,LAST_VALUE([SUM_Amount in LC]) 
+　　　OVER(PARTITION BY [col1], [col2] ORDER BY [Date] ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS [MaxDayAmount]
+FROM
+　tmp_tbl
+;
